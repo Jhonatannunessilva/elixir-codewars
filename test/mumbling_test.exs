ff868f9f2458a7bbae7cb30cb54ca934bf5f590d
Jhonatan Nunes da Silva <jhonatannunessilva@hotmail.com>
2021-02-24 12:59:19 -0300

    mumbling resolved

---
+++ test/mumbling_test.exs
@@ -0,0 +1,20 @@
+defmodule MumblingTest do
+  use ExUnit.Case
+
+  test "basic" do
+    assert Mumbling.accum("ZpglnRxqenU") ==
+             "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+
+    assert Mumbling.accum("NyffsGeyylB") ==
+             "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+
+    assert Mumbling.accum("MjtkuBovqrU") ==
+             "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+
+    assert Mumbling.accum("EvidjUnokmM") ==
+             "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+
+    assert Mumbling.accum("HbideVbxncC") ==
+             "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
+  end
+end
