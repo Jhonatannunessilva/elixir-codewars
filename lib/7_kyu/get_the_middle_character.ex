b56cc91451478d0fd6750c1b369ae071c607de39
Jhonatan Nunes da Silva <jhonatannunessilva@hotmail.com>
2021-02-25 08:32:04 -0300

    get_the_middle_character refactored

--- lib/7_kyu/get_the_middle_character.ex
+++ lib/7_kyu/get_the_middle_character.ex
@@ -1,13 +1,6 @@
 defmodule GetTheMiddleCharacter do
-  require Integer
-
   def get_middle(str) do
-    len = String.length(str)
-    middle = div(len, 2)
-
-    case Integer.is_odd(len) do
-      true -> String.at(str, middle)
-      _ -> String.slice(str, (middle - 1)..middle)
-    end
+    middle = (String.length(str) - 1) / 2
+    String.slice(str, trunc(middle)..round(middle))
   end
 end
