e99dc46ed2c918fcec3acf31947b638805155e3e
Jhonatan Nunes da Silva <jhonatannunessilva@hotmail.com>
2021-02-28 15:37:40 -0300

    printer_errors refactored

--- lib/7_kyu/printer_errors.ex
+++ lib/7_kyu/printer_errors.ex
@@ -1,11 +1,12 @@
 defmodule PrinterErrors do
   def printer_error(s) do
-    errors =
-      s
-      |> String.to_charlist()
-      |> Enum.filter(&(&1 in ?n..?z))
-      |> Enum.count()
+    "#{error_count(s)}/#{String.length(s)}"
+  end
 
-    "#{errors}/#{String.length(s)}"
+  defp error_count(s) do
+    s
+    |> String.graphemes()
+    |> Enum.filter(&(&1 > "m"))
+    |> Enum.count()
   end
 end
